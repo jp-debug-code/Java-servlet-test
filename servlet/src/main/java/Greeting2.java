4239ad5a5669f693aee5a1c1fc33a4e6305fb6c7
jp-debug-code <kiminoakari@gmail.com>
2023-10-28 04:42:24 +0900

    28日コミット

---
+++ servlet/src/main/java/Greeting2.java
@@ -0,0 +1,88 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Greeting
+ */
+@WebServlet("/chapter5/greeting2")
+public class Greeting2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Greeting2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out=response.getWriter();
+		request.setCharacterEncoding("UTF-8");
+		String user=request.getParameter("user");
+		Page.header(out);
+		out.println("<p>こんにちは"+user+" さん");
+		Page.footer(out);
+
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out=response.getWriter();
+		request.setCharacterEncoding("UTF-8");
+		String user=request.getParameter("user");
+		Page.header(out);
+		out.println("<p>こんにちは"+user+"さん");
+		Page.footer(out);
+
+	}
+
+}
+
+//
+//
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//@WebServlet(urlPatterns={"/chapter5/greeting"})
+//public class Greeting extends HttpServlet {
+//
+//	public void doGet (
+//		HttpServletRequest request, HttpServletResponse response
+//	) throws ServletException, IOException {
+//		response.setContentType("text/html; charset=UTF-8");
+//		PrintWriter out=response.getWriter();
+//
+//		request.setCharacterEncoding("UTF-8");
+//		String user=request.getParameter("user");
+//
+//		Page.header(out);
+//		out.println("<p>こんにちは、"+user+"さん！</p>");
+//		Page.footer(out);
+//	}
+//}
